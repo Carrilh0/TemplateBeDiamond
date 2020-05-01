5bbbf899a861126b397460847d35f5d3126de72e
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-05-01 04:29:24 -0300

    fixs

--- js/main.js
+++ js/main.js
@@ -366,3 +366,12 @@ mailChimp();
     });
 
 })(jQuery);	
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
